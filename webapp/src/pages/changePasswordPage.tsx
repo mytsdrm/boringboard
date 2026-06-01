f5e50236450ce4d4b8271ca5d6e946f1dfcc1c24
andibastian <andibastian@thejagat.com>
2026-06-01 09:00:13 +0700

    update UI change password

--- webapp/src/pages/changePasswordPage.tsx
+++ webapp/src/pages/changePasswordPage.tsx
@@ -4,6 +4,9 @@ import React, {useState} from 'react'
 import {Link} from 'react-router-dom'
 
 import Button from '../widgets/buttons/button'
+import IconButton from '../widgets/buttons/iconButton'
+import HideIcon from '../widgets/icons/hide'
+import ShowIcon from '../widgets/icons/show'
 import client from '../octoClient'
 import './changePasswordPage.scss'
 import {IUser} from '../user'
@@ -13,6 +16,8 @@ import {getMe} from '../store/users'
 const ChangePasswordPage = () => {
     const [oldPassword, setOldPassword] = useState('')
     const [newPassword, setNewPassword] = useState('')
+    const [showOldPassword, setShowOldPassword] = useState(false)
+    const [showNewPassword, setShowNewPassword] = useState(false)
     const [errorMessage, setErrorMessage] = useState('')
     const [succeeded, setSucceeded] = useState(false)
     const user = useAppSelector<IUser|null>(getMe)
@@ -20,13 +25,34 @@ const ChangePasswordPage = () => {
     if (!user) {
         return (
             <div className='ChangePasswordPage'>
-                <div className='title'>{'Change Password'}</div>
-                <Link to='/login'>{'Log in first'}</Link>
+                <div className='card'>
+                    <div className='brand'>
+                        <img
+                            src='/static/boringboard-logo.webp'
+                            alt='BoringBoard'
+                        />
+                    </div>
+                    <div className='title'>{'Change password'}</div>
+                    <Link to='/login'>{'Log in first'}</Link>
+                </div>
             </div>
         )
     }
 
     const handleSubmit = async (userId: string): Promise<void> => {
+        if (!oldPassword && !newPassword) {
+            setErrorMessage('Please enter your current and new password.')
+            return
+        }
+        if (!oldPassword) {
+            setErrorMessage('Please enter your current password.')
+            return
+        }
+        if (!newPassword) {
+            setErrorMessage('Please enter your new password.')
+            return
+        }
+
         const response = await client.changePassword(userId, oldPassword, newPassword)
         if (response.code === 200) {
             setOldPassword('')
@@ -40,36 +66,73 @@ const ChangePasswordPage = () => {
 
     return (
         <div className='ChangePasswordPage'>
-            <div className='title'>{'Change Password'}</div>
             <form
                 onSubmit={(e: React.FormEvent) => {
                     e.preventDefault()
                     handleSubmit(user.id)
                 }}
             >
-                <div className='oldPassword'>
-                    <input
-                        id='login-oldpassword'
-                        type='password'
-                        placeholder={'Enter current password'}
-                        value={oldPassword}
-                        onChange={(e) => {
-                            setOldPassword(e.target.value)
-                            setErrorMessage('')
-                        }}
+                <div className='brand'>
+                    <img
+                        src='/static/boringboard-logo.webp'
+                        alt='BoringBoard'
                     />
                 </div>
+                {errorMessage &&
+                    <div className='error'>
+                        {errorMessage}
+                    </div>
+                }
+                {succeeded &&
+                    <Link
+                        className='succeeded'
+                        to='/'
+                    >{'Password changed. Continue to BoringBoard.'}</Link>
+                }
+                <div className='title'>{'Change password'}</div>
+                <div className='oldPassword'>
+                    <label htmlFor='login-oldpassword'>{'Current password'}</label>
+                    <div className='passwordField'>
+                        <input
+                            id='login-oldpassword'
+                            type={showOldPassword ? 'text' : 'password'}
+                            placeholder={'Enter current password'}
+                            value={oldPassword}
+                            onChange={(e) => {
+                                setOldPassword(e.target.value)
+                                setErrorMessage('')
+                                setSucceeded(false)
+                            }}
+                        />
+                        <IconButton
+                            className='togglePassword'
+                            title={showOldPassword ? 'Hide password' : 'Show password'}
+                            icon={showOldPassword ? <HideIcon/> : <ShowIcon/>}
+                            onClick={() => setShowOldPassword(!showOldPassword)}
+                        />
+                    </div>
+                </div>
                 <div className='newPassword'>
-                    <input
-                        id='login-newpassword'
-                        type='password'
-                        placeholder={'Enter new password'}
-                        value={newPassword}
-                        onChange={(e) => {
-                            setNewPassword(e.target.value)
-                            setErrorMessage('')
-                        }}
-                    />
+                    <label htmlFor='login-newpassword'>{'New password'}</label>
+                    <div className='passwordField'>
+                        <input
+                            id='login-newpassword'
+                            type={showNewPassword ? 'text' : 'password'}
+                            placeholder={'Enter new password'}
+                            value={newPassword}
+                            onChange={(e) => {
+                                setNewPassword(e.target.value)
+                                setErrorMessage('')
+                                setSucceeded(false)
+                            }}
+                        />
+                        <IconButton
+                            className='togglePassword'
+                            title={showNewPassword ? 'Hide password' : 'Show password'}
+                            icon={showNewPassword ? <HideIcon/> : <ShowIcon/>}
+                            onClick={() => setShowNewPassword(!showNewPassword)}
+                        />
+                    </div>
                 </div>
                 <Button
                     filled={true}
@@ -77,21 +140,10 @@ const ChangePasswordPage = () => {
                 >
                     {'Change password'}
                 </Button>
+                {!succeeded &&
+                    <Link to='/'>{'Cancel'}</Link>
+                }
             </form>
-            {errorMessage &&
-                <div className='error'>
-                    {errorMessage}
-                </div>
-            }
-            {succeeded &&
-                <Link
-                    className='succeeded'
-                    to='/'
-                >{'Password changed, click to continue.'}</Link>
-            }
-            {!succeeded &&
-                <Link to='/'>{'Cancel'}</Link>
-            }
         </div>
     )
 }
