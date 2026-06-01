f753014bec4906df96c13127a98ee71e1e633beb
andibastian <andibastian@thejagat.com>
2026-06-01 08:14:25 +0700

    update login UI

--- webapp/src/pages/loginPage.tsx
+++ webapp/src/pages/loginPage.tsx
@@ -8,12 +8,16 @@ import {useAppDispatch, useAppSelector} from '../store/hooks'
 import {fetchMe, getLoggedIn} from '../store/users'
 
 import Button from '../widgets/buttons/button'
+import IconButton from '../widgets/buttons/iconButton'
+import HideIcon from '../widgets/icons/hide'
+import ShowIcon from '../widgets/icons/show'
 import client from '../octoClient'
 import './loginPage.scss'
 
 const LoginPage = () => {
     const [username, setUsername] = useState('')
     const [password, setPassword] = useState('')
+    const [showPassword, setShowPassword] = useState(false)
     const [errorMessage, setErrorMessage] = useState('')
     const dispatch = useAppDispatch()
     const loggedIn = useAppSelector<boolean|null>(getLoggedIn)
@@ -21,7 +25,21 @@ const LoginPage = () => {
     const history = useHistory()
 
     const handleLogin = async (): Promise<void> => {
-        const logged = await client.login(username, password)
+        const trimmedUsername = username.trim()
+        if (!trimmedUsername && !password) {
+            setErrorMessage('Please enter your username and password.')
+            return
+        }
+        if (!trimmedUsername) {
+            setErrorMessage('Please enter your username.')
+            return
+        }
+        if (!password) {
+            setErrorMessage('Please enter your password.')
+            return
+        }
+
+        const logged = await client.login(trimmedUsername, password)
         if (logged) {
             await dispatch(fetchMe())
             if (queryParams) {
@@ -30,7 +48,7 @@ const LoginPage = () => {
                 history.push('/')
             }
         } else {
-            setErrorMessage('Login failed')
+            setErrorMessage('Username or password is incorrect.')
         }
     }
 
@@ -46,16 +64,22 @@ const LoginPage = () => {
                     handleLogin()
                 }}
             >
-                <div className='title'>
-                    <FormattedMessage
-                        id='login.log-in-title'
-                        defaultMessage='Log in'
+                <div className='brand'>
+                    <img
+                        src='/static/boringboard-logo.webp'
+                        alt='BoringBoard'
                     />
                 </div>
+                {errorMessage &&
+                    <div className='error'>
+                        {errorMessage}
+                    </div>
+                }
                 <div className='username'>
+                    <label htmlFor='login-username'>{'Username'}</label>
                     <input
                         id='login-username'
-                        placeholder={'Enter username'}
+                        placeholder={'Enter your username'}
                         value={username}
                         onChange={(e) => {
                             setUsername(e.target.value)
@@ -64,16 +88,25 @@ const LoginPage = () => {
                     />
                 </div>
                 <div className='password'>
-                    <input
-                        id='login-password'
-                        type='password'
-                        placeholder={'Enter password'}
-                        value={password}
-                        onChange={(e) => {
-                            setPassword(e.target.value)
-                            setErrorMessage('')
-                        }}
-                    />
+                    <label htmlFor='login-password'>{'Password'}</label>
+                    <div className='passwordField'>
+                        <input
+                            id='login-password'
+                            type={showPassword ? 'text' : 'password'}
+                            placeholder={'Enter your password'}
+                            value={password}
+                            onChange={(e) => {
+                                setPassword(e.target.value)
+                                setErrorMessage('')
+                            }}
+                        />
+                        <IconButton
+                            className='togglePassword'
+                            title={showPassword ? 'Hide password' : 'Show password'}
+                            icon={showPassword ? <HideIcon/> : <ShowIcon/>}
+                            onClick={() => setShowPassword(!showPassword)}
+                        />
+                    </div>
                 </div>
                 <Button
                     filled={true}
@@ -84,18 +117,13 @@ const LoginPage = () => {
                         defaultMessage='Log in'
                     />
                 </Button>
+                <Link to='/register'>
+                    <FormattedMessage
+                        id='login.register-button'
+                        defaultMessage={'Create an account'}
+                    />
+                </Link>
             </form>
-            <Link to='/register'>
-                <FormattedMessage
-                    id='login.register-button'
-                    defaultMessage={'or create an account if you don\'t have one'}
-                />
-            </Link>
-            {errorMessage &&
-                <div className='error'>
-                    {errorMessage}
-                </div>
-            }
         </div>
     )
 }
